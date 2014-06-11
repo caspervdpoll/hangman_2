e070be0897a19bafc506978befa4c4260ed9eb78
Casper van der Poll <casper.vanderpoll@gmail.com>
2014-06-11 18:47:06 +0200

    functie voor letter moet alleen nog een selector voor de grootste rij

--- app/twisted-hangman/src/com/example/twisted_hangman/singleplayerActivity.java
+++ app/twisted-hangman/src/com/example/twisted_hangman/singleplayerActivity.java
@@ -2,7 +2,9 @@ package com.example.twisted_hangman;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.twisted_hangman.sqlite.User;
 import com.example.twisted_hangman.sqlite.Words_nl;
@@ -44,9 +46,27 @@ public class singleplayerActivity extends ActionBarActivity {
    	
    	public ArrayList<String> filterOnChar(ArrayList<String> words, char input) {
    		ArrayList<String> result = new ArrayList<String>();
+   		Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+   		for(int i = 0; i < words.size(); i++){
+   			String key = "";
+   			for(int j = 0; j < words.get(i).length(); j++){
+   				char compare = words.get(i).charAt(j);
+   				if(compare == input)
+   					key += "1";
+   				else
+   					key += "0";
+   			}
+   			if(map.containsKey(key)){
+   				ArrayList<String> temp = map.get(key);
+   				temp.add(words.get(i));
+   	   			map.put(key, temp);
+   			} else {
+   				ArrayList<String> temp = new ArrayList<String>();
+   				map.put(key, temp);
+   			}
+   		}
    		
-   		
-   		
+   		System.out.println(map.keySet());
    		return result;
    	}
    	
@@ -61,7 +81,7 @@ public class singleplayerActivity extends ActionBarActivity {
    				no.add(words.get(i));
    			}
    		}
-   		
+   		System.out.println("yolo");
    		if(yes.size() > no.size()) {
    			result = filterOnChar(yes, input);
    			return result;
