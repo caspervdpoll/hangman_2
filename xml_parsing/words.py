bb0d30a39a3d9ff443c88fd055e13e834ba03807
Juvawa <justinvwageningen@gmail.com>
2014-06-27 20:49:26 +0200

    ff tussendoor

--- xml_parsing/words.py
+++ xml_parsing/words.py
@@ -1,20 +1,8 @@
-f = open('24.txt', 'w');
-words = open('words.txt', 'r')
-query = "INSERT INTO WORDS_24 (id, value) VALUES "
-counter = 0
-print_counter = 0
-for word in words.read().split("\n"):
-	#print word
-	if len(word) == 24:
-		counter += 1
-		print_counter += 1
-		if counter == 300:
-			counter = 1
-			query += ";\n"
-			query += "INSERT INTO WORDS_24 (id, value) VALUES "
-		query += "(" + str(print_counter) + ", '" + word + "'), "
-
-query += ";"
-f.write(query);
+f = open('ids.txt', 'w');
+words = open('series.txt', 'r')
+for word in words.read().split(";"):
+	word = word + "\n";
+	print word
+	f.write(word);
 f.close()
 words.close()
